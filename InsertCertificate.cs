4b13c5bdf54bdcb500363e23069f05f3c7f06121
jcbrooks <lilman_2010@hotmail.com>
2021-02-21 19:19:01 -0500

    2/21 Update - Parallelism
    2/21 Update - Parallelism
    
    1. Added ParallelForEach to most loops
    2. One questionable set of code on 126 that could cause blocking but it
    has increased overall throughput -  cert =  GetCertInformation(urlValue,
    cert, log).Result;

--- InsertCertificate.cs
+++ InsertCertificate.cs
@@ -19,6 +19,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Azure.Cosmos.Fluent;
 using Newtonsoft.Json.Converters;
 using System.Security.Cryptography;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace CertificateCheckerFunctionApp
 {
@@ -52,12 +55,17 @@ namespace CertificateCheckerFunctionApp
             ILogger log)
         {
             //Declare Variables
-            IList<CertInformation> certList = new List<CertInformation>();
-            IList<string> url = new List<string>();
+            //IList<CertInformation> certList = new List<CertInformation>();
+            //IList<string> url = new List<string>();
+            ConcurrentBag<CertInformation> certList = new ConcurrentBag<CertInformation>();
+            ConcurrentBag<string> url = new ConcurrentBag<string>();
 
-            
+
             log.LogInformation("C# HTTP trigger function processed a request.");
             var requestBodyJson = await new StreamReader(req.Body).ReadToEndAsync();
+            Stopwatch timer = new Stopwatch();
+            //timer.Start();
+
             try
             {
                 //Check if body contains URLS to check
@@ -75,17 +83,22 @@ namespace CertificateCheckerFunctionApp
                 {
                     url.Add( req.Query["url"]);
                 }
-                else return new BadRequestObjectResult("No value passed");
+                else return new BadRequestObjectResult("No value passed in header or body.");
             }
             catch (Exception e)
             {
                 log.LogError($"Exception occurred: {e}");
                 return new BadRequestObjectResult($"Failed to get parsed cert from header or body with exception{e}");
             }
+            //timer.Stop();
+            //log.LogInformation($"Pulling body {timer.ElapsedMilliseconds} ms");
 
+            //timer.Reset();
+            timer.Start();
             //Logic for checking if URL is the correct format
-            //
-            foreach (string hostname in url){
+            //foreach (string hostname in url)
+            Parallel.ForEach(url, async(hostname) =>
+            { 
                 try
                 {
                     await Dns.GetHostEntryAsync(hostname);
@@ -93,47 +106,69 @@ namespace CertificateCheckerFunctionApp
                 catch(Exception e)
                 {
                     log.LogError($"DNS resolution for {hostname} failed with {e}");
-                    return new BadRequestObjectResult($"Dns Resolution failed for {hostname}. Please fix or remove this URL and resubmit");
+                    //return new BadRequestObjectResult($"Dns Resolution failed for {hostname}. Please fix or remove this URL and resubmit");
                 }
-            }
+            });
+            timer.Stop();
+            log.LogInformation($"Validating DNS {timer.ElapsedMilliseconds} ms");
 
             //Foreach URL try to get Cert information
-            foreach (string urlValue in url)
-            {
-                CertInformation cert = new CertInformation();
-                try
-                {
+            timer.Reset();
+            timer.Start();
+            //foreach (string urlValue in url)
+            Parallel.ForEach(url, async(urlValue) =>
+             {
+                 CertInformation cert = new CertInformation();
+                 try
+                 {
                     //Creating cert object with properties 
-                    cert = await GetCertInformation(urlValue, cert, log);
+                    ///Very questionable code that will probably cause blocking/deadlocking at some point......Trying to optimize calls to be parallel to spped up execution time. If any deadlocking issues revert back to foreach
+                    cert =  GetCertInformation(urlValue, cert, log).Result;
                     cert.Region = Environment.GetEnvironmentVariable("region");
                     certList.Add(cert);
-                }
-                catch (Exception e)
-                {
-                    log.LogError($"Exception Occurred trying to pull certificate for {urlValue} : {e}");
-                    return new BadRequestObjectResult($"Failed to get parse cert with exception {e}");
-                }
-            }
-            //Convert to JSON 
+                 }
+                 catch (Exception e)
+                 {
+                     log.LogError($"Exception Occurred trying to pull certificate for {urlValue} : {e}");
+                     //return new BadRequestObjectResult($"Failed to get parse cert with exception {e}");
+                 }
+             });
+
+            timer.Stop();
+            log.LogInformation($"Total time for all certs info {timer.ElapsedMilliseconds} ms");
+
+            //Convert to JSON
             string jsonString = System.Text.Json.JsonSerializer.Serialize(certList);
-            foreach (var certItem in certList)
+
+            timer.Reset();
+            timer.Start();
+            //foreach (var certItem in certList)
+            Parallel.ForEach(certList, async (certItem) =>
             {
                 try
                 {
                     await document.AddAsync(certItem);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     log.LogError($"Failed to update DB for {certItem.URL} with exception: {e}");
                 }
-        }
-
+            });
+            timer.Stop();
+            log.LogInformation($"Final Step {timer.ElapsedMilliseconds} ms");
             return new OkObjectResult(jsonString);
         }
-
+        //Creating Certificate properties to send to cosmos
         public static async Task<CertInformation> GetCertInformation(string urlValue, CertInformation cert, ILogger log)
         {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
             X509Certificate2 certObject = await GetServerCertificateAsync("https://" + urlValue);
+            timer.Stop();
+            log.LogInformation($"Getting Cert {urlValue} {timer.ElapsedMilliseconds} ms");
+
+            //timer.Reset();
+            //timer.Start();
             cert.id = urlValue;
             cert.URL = certObject.Subject;
             cert.CertExpiration = certObject.GetExpirationDateString();
@@ -144,7 +179,7 @@ namespace CertificateCheckerFunctionApp
             var TimeTilExpiration = IsCertificateExpiring(cert, log);
             cert.TimeTilExpiration = TimeTilExpiration.Days.ToString();
 
-            //Pull the SAN Certificate https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.x509certificates.x509nametype?view=net-5.0
+            //Pull the SAN from certificate https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.x509certificates.x509nametype?view=net-5.0
             foreach (X509Extension extension in certObject.Extensions)
             {
                 if (extension.Oid.FriendlyName.Equals("Subject Alternative Name"))
@@ -165,18 +200,22 @@ namespace CertificateCheckerFunctionApp
                 cert.IsExpired = true;
                 log.LogInformation($"Cert for {cert.URL} EXPIRED {TimeTilExpiration.TotalDays} days ago.");
             }
+            //timer.Stop();
+            //log.LogInformation($"Creating certObject for {urlValue} {timer.ElapsedMilliseconds} ms");
+
             return cert;
         }
 
+        //Checking cert expiration
         static TimeSpan IsCertificateExpiring(CertInformation cert, ILogger log)
         {
             TimeSpan expirationTime = DateTime.Parse(cert.CertExpiration) - DateTime.Now;
-            TimeSpan test = DateTime.Now - DateTime.Parse(cert.CertExpiration);
             return expirationTime;
         }
-
+        //Pulling Cert from HTTPS Request from url
         static async Task<X509Certificate2> GetServerCertificateAsync(string url)
         {
+
             X509Certificate2 certificate = null;
             var httpClientHandler = new HttpClientHandler
             {
@@ -188,7 +227,8 @@ namespace CertificateCheckerFunctionApp
             };
                 httpClient = new HttpClient(httpClientHandler);
                 await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
-                return certificate ?? throw new NullReferenceException();
+
+            return certificate ?? throw new NullReferenceException();
         }
     }
 }
